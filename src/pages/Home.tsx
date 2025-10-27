d40cf035a3e04ca19ae2957b6c30c788bdc1c76b
SHONG0504 <seungminhong2002@gmail.com>
2025-10-26 23:38:58 -0400

    Cleaned structure

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import './Home.css';
+
+function Home() {
+    return (
+        <main className='home'>
+            <h1 className="silkscreen-regular">Simantic</h1>
+            <h2 className="silkscreen-regular">Test hardware without hardware</h2>
+            <h3 className="silkscreen-regular">Coming soon</h3>
+        </main>
+    )
+}
+
+export default Home;
