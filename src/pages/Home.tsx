405603c528678a4d86201ba976d81d26f72a506c
SHONG0504 <seungminhong2002@gmail.com>
2025-10-27 03:05:25 -0400

    Spacing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,13 +2,13 @@ import './Home.css';
 import { Link } from 'react-router-dom';
 
 export default function Home() {
-    return (
-        <main className='home'>
-            <h1 className="silkscreen-regular">Simantic</h1>
-            <h2 className="silkscreen-regular">Test hardware without hardware</h2>
-            <h3 className="silkscreen-regular">
-                <Link to="/waitlist" className="join-waitlist-button">Join the Waitlist</Link>
-            </h3>
-        </main>
-    )
+  return (
+    <main className='home'>
+      <h1 className="silkscreen-regular">Simantic</h1>
+      <h2 className="silkscreen-regular">Test hardware without hardware</h2>
+      <h3 className="silkscreen-regular">
+        <Link to="/waitlist" className="join-waitlist-button silkscreen-thin">Join the Waitlist</Link>
+      </h3>
+    </main>
+  )
 }
