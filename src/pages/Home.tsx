0b7a9d1c826f3961974b27251745ea75f8866f47
SHONG0504 <seungminhong2002@gmail.com>
2025-10-27 02:55:40 -0400

    Waitlist button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,14 @@
 import './Home.css';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
     return (
         <main className='home'>
             <h1 className="silkscreen-regular">Simantic</h1>
             <h2 className="silkscreen-regular">Test hardware without hardware</h2>
-            <h3 className="silkscreen-regular">Coming soon</h3>
+            <h3 className="silkscreen-regular">
+                <Link to="/waitlist" className="join-waitlist-button">Join the Waitlist</Link>
+            </h3>
         </main>
     )
 }
